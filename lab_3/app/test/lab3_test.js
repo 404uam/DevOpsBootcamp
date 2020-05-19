d485cec9ec8581e3d242fe2e6c8f7fe64368a14c
Louis Mau <404uam@users.noreply.github.com>
2020-05-19 11:33:25 -0600

    Update lab3_test.js

--- lab_3/app/test/lab3_test.js
+++ lab_3/app/test/lab3_test.js
@@ -10,7 +10,7 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-const expectedResponseText = "Hello, World!";
+const expectedResponseText = "Hello, World!222";
 
 describe("API test", function () {
   it("should returns '" + expectedResponseText + "'", (done) => {
